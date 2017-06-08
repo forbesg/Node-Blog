373d6fc8317e4f71ff13daeec045254c735cf011
Forbes Gray <fnbg75@gmail.com>
2017-06-08 09:36:24 +0100

    Change the way sticky nav is measured (.nav-bar offsetTop) for a more consistent cross browser result

--- client/js/script.js
+++ client/js/script.js
@@ -1,8 +1,10 @@
 (function() {
   console.log('Script Loaded');
   const header = document.querySelector('.header');
+  const navBar = document.querySelector('.nav-bar');
+  const originalNavOffset = navBar.offsetTop;
   window.onscroll = () => {
-    if (pageYOffset >= 202) {
+    if (pageYOffset >= originalNavOffset) {
       return header.classList.add('fixed-nav');
     }
     header.classList.remove('fixed-nav');
