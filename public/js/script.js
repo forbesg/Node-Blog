59e6eadc79b05ee1c8bfd78eee15473d18c7624a
Forbes Gray <fnbg75@gmail.com>
2017-06-15 11:05:18 +0100

    Remove arrow functions for legacy browsers and IE fix for fixed background jitter

--- public/js/script.js
+++ public/js/script.js
@@ -4,20 +4,37 @@
   const navBar = document.querySelector('.nav-bar');
   const originalNavOffset = navBar.offsetTop;
   const clearButton = document.querySelector('.clear-button');
-  window.onscroll = () => {
+
+  // Set Fixed class to nav bar
+  function handleScroll() {
+    console.log('scroll');
     if (pageYOffset >= originalNavOffset) {
       return header.classList.add('fixed-nav');
     }
     header.classList.remove('fixed-nav');
   }
-  window.onkeydown = (e) => {
+
+  window.addEventListener('scroll', handleScroll);
+  if(navigator.userAgent.match(/Trident\/7\./)) { // if IE
+    window.addEventListener("mousewheel", function (event) {
+      // remove default behavior
+      event.preventDefault();
+
+      //scroll without smoothing
+      var wheelDelta = event.wheelDelta;
+      var currentScrollPosition = window.pageYOffset;
+      window.scrollTo(0, currentScrollPosition - wheelDelta);
+    });
+  }
+
+  window.onkeydown = function (e) {
     if (e.keyCode === 83 && e.ctrlKey) {
         e.preventDefault();
         console.log('Save?');
     }
   }
   if (clearButton) {
-    clearButton.addEventListener('click', (e) => {
+    clearButton.addEventListener('click', function(e) {
       e.preventDefault();
       e.target.parentNode.remove();
     })
